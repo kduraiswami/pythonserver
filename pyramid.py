60db17670b75b3441fdc02cc3953f6579852b384
kduraiswami <kaidurai@gmail.com>
2016-09-19 20:41:08 -0700

    trying to use importlib util

--- pyramid.py
+++ pyramid.py
@@ -1,10 +1,18 @@
 
 import sys
-sys.path.insert(0, "/Library/Frameworks/Python.framework/Versions/3.5/lib/python3.5/site-packages/pyramid/__init__.py")
+sys.path.insert(0, "/Library/Frameworks/Python.framework/Versions/3.5/lib/python3.5/site-packages/")
 
-import pyramid
+import importlib.util
+
+
+spec = importlib.util.spec_from_file_location("/Library/Frameworks/Python.framework/Versions/3.5/lib/python3.5/site-packages/pyramid/pyramid.py")
+mod = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(mod)
+
 import pdb
 pdb.set_trace()
+
+import mod.pyramid
 
 from pyramid.config import Configurator
 from pyramid.response import Response
